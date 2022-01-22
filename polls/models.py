ff22ff32c90143bdcdd4baffff6e1420197bf638
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-01-22 13:20:03 -0700

    added custom method to Question class

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+import django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -6,6 +8,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
