602085262e0f03176e96cef1410208548d0e36a5
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-01-22 12:46:12 -0700

    added first polls view and created urls.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Hello, world! Polls here we come.")
 # Create your views here.
