73d1e4471cdcf3d837323406f5ef73e0dedcfa40
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-01-22 13:13:22 -0700

    created db tables based off models

--- polls/models.py
+++ polls/models.py
@@ -7,4 +7,4 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = modesl.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
