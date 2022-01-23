400f178b157a21a4787ebff6fc4a8b2be3aa2e88
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-01-22 17:03:09 -0700

    updated index view

--- polls/views.py
+++ polls/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! Polls here we come.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    respone = "You're looking at the results of question %s."
+    response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
