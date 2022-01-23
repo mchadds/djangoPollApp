78bd2a26f250109aaa25d504c2378f80928ccad0
Chad Dundas Smith <chaddundassmith@Chads-MacBook-Pro.local>
2022-01-22 18:02:13 -0700

    fixed imports for views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question
 
